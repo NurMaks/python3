8960aaaf830cca154ca555d7aec96cbf6a861aa5
nurtaims <nurtaims@gmail.com>
2018-10-30 00:13:14 +0600

    ticket booking system

---
+++ project5 tickets booking with SQLite/kino.py
@@ -0,0 +1,33 @@
+from DB import DB
+from times import Times
+
+class Kino:
+    def __init__(self, cinemaID):
+        self.kino = []
+        for db in DB().select("Kino", "*"):
+            arr = {}
+            arr["id"] = db[0]
+            arr["name"] = db[1]
+            self.kino.append(arr)
+        self.chosenID = None
+        self.cinemaID = cinemaID
+        self.time = None #Times(cinemaID, self.id)
+    
+    def getKino(self):
+        return self.kino
+    
+    def setChosenID(self, id):
+        self.chosenID = id
+        self.time = Times(self.cinemaID, self.chosenID)
+
+    def setChosenTime(self, timeID):
+        self.time.setChosenTime(timeID)
+
+    def getTimes(self):
+        return self.time.getTimes()
+
+    def getPlaces(self):
+        return self.time.getPlace()
+    
+    def setChosenPlaces(self, place):
+        self.time.setChosenPlaces(place)
---
+++ project6 tickets booking with PostgreSQL/place.py
@@ -0,0 +1,21 @@
+from DB import DB
+
+class Place:
+    def __init__(self, timeID):
+        self.place = []
+        for db in DB().select("Hall", "*", "time_id="+str(timeID)):
+            arr = {}
+            arr["id"] = db[0]
+            arr['place'] = db[2]
+            arr['status'] = db[3]
+            self.place.append(arr)
+    
+    def getPlaces(self):
+        return self.place
+
+    def setChosenPlaces(self, place):
+        self.chosenPlaces = place
+        db = DB()
+        for pl in self.chosenPlaces:
+            id = self.place[0]['id']+pl-1
+            db.change("Hall", "status=1", "hall_id="+str(id))
